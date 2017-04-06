2d9ec09c4f78da258b7f1b42a0e2d2ce5ecdb31e
Trevor Allen <trevor.allen@aquicore.com>
2017-04-05 22:52:16 -0400

    add simple cursor movement

--- spicy.c
+++ spicy.c
@@ -14,6 +14,7 @@
 
 /* data */
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -148,7 +149,10 @@ void editorRefreshScreen() {
 
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -156,6 +160,23 @@ void editorRefreshScreen() {
 }
 
 /* input */
+void editorMoveCursor(char key) {
+    switch (key) {
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
+}
+
 void editorProcessKeypress() {
     char c = editorReadKey();
 
@@ -165,11 +186,20 @@ void editorProcessKeypress() {
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+        case 'w':
+        case 'a':
+        case 's':
+        case 'd':
+            editorMoveCursor(c);
+            break;
     }
 }
 
 /* init */
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
